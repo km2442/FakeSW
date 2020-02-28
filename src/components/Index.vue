5575e8beffb64ee0d8ccce1e188291d4f1c22243
Kamil <urlikodd@gmail.com>
2020-02-28 22:37:05 +0100

    Added Login page

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <p class="display-3 font-weight-bold text-center ma-3">Zaloguj się</p>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6 lg4>
+          <v-card outlined>
+            <v-card-text>
+              <v-form>
+                <v-text-field label="Login" prepend-icon="mdi-account" type="text"></v-text-field>
+                <v-text-field label="Hasło" prepend-icon="mdi-lock" type="password"></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-hover v-slot:default="{ hover }">
+                <v-btn large block color="primary" :outlined="hover ? false : true">Wyślij</v-btn>
+              </v-hover>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-    data: () => ({
+  data: () => ({
     //
-  }),
-}
+  })
+};
 </script>
