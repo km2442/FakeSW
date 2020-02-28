d1ba2ae1766fdc1211823d1962f28e32fabf8571
Kamil <urlikodd@gmail.com>
2020-02-28 22:46:56 +0100

    Added theme changer

---
+++ src/components/ThemeChanger.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-hover v-slot:default="{ hover }">
+      <v-btn
+        :light="$vuetify.theme.dark ? false : true"
+        :dark="$vuetify.theme.dark ? true : false"
+        :outlined="hover ? false : true"
+        :color="$vuetify.theme.dark ? 'white' : 'black'"
+        fab
+        fixed
+        depressed
+        bottom
+        right
+        @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+      >
+        <v-icon
+          x-large
+          :color="hover ? $vuetify.theme.dark ? 'black' : 'white' : $vuetify.theme.dark ? 'white' : 'black'"
+        >mdi-brightness-6</v-icon>
+      </v-btn>
+    </v-hover>
+  </div>
+</template>
