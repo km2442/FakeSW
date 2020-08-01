d51698314863f8af9137db4848ec50cf0ae06d43
Kamil <urlikodd@gmail.com>
2020-08-01 22:19:18 +0200

    Changed year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,7 @@
   <v-footer absolute dark color="primary" class="font-weight-medium">
     <v-col class="text-center" cols="12">
       &copy;
-      <strong>Kamil Klecha</strong>
-      {{ new Date().getFullYear() }}
+      <strong>Kamil Klecha</strong> 2019
     </v-col>
   </v-footer>
 </template>
