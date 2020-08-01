5be9f8e15eca1d605e92c62879017c72cd1b410e
Kamil <urlikodd@gmail.com>
2020-08-01 22:49:44 +0200

    Fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer absolute dark color="primary" class="font-weight-medium">
+  <v-footer dark color="primary" class="font-weight-medium">
     <v-col class="text-center" cols="12">
       &copy;
       <strong>Kamil Klecha</strong> 2019
